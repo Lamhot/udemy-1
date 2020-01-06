800d56e3703b4fe837e3fe21aa684ab8a6cd9f11
Lamhotjm <lamhotjms@gmail.com>
2020-01-06 15:05:47 -0600

    chapter 7

--- src/ajax/chapter7/js/script.js
+++ src/ajax/chapter7/js/script.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
 	//our code goes
 	//$('#container').text('hello');
 	//$('.class1').text('hello');
-	$('.class1,.class2').addClass('highlighted');
+	//$('.class1,.class2').addClass('highlighted');
+	// if the element li not directly after listing then  > should not be covered
+	$('#listing > li').addClass('highlighted');
 });
