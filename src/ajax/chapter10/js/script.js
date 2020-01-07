e7b0fe0f933e4acc1fa976aec9988532ac4ccdf0
Lamhotjm <lamhotjms@gmail.com>
2020-01-07 05:14:02 -0600

    add chapter 10

---
+++ src/ajax/chapter10/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	//our code goes
+	//$('#container').text('hello');
+	//$('.class1').text('hello');
+	//$('.class1,.class2').addClass('highlighted');
+	// if the element li not directly after listing then  > should not be covered
+//	$('#listing > li').addClass('highlighted');
+	//$('input[required]').addClass('highlighted');
+	//$('input[placeholder=Email]').addClass('highlighted');
+	//$('input[placeholder*=Name]').addClass('highlighted');
+	// var result= $('#animals .creature');
+	// //console.log(result);
+	// result = $('#animals').find('.creature');
+	// console.log(result);
+//	var result = $('#animals > .creature');
+
+	var result= $('#animals').children('.creature');
+	console.log(result);
+});
