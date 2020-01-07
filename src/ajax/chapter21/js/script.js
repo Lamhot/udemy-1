538d2fa06152f6231ca5ca26de66178a70fcfb54
Lamhotjm <lamhotjms@gmail.com>
2020-01-07 15:25:06 -0600

    init chapter 21

---
+++ src/ajax/chapter21/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('#add-container').on('click', 'button', function(){
+		var value = $('#add-container input').val();
+
+		var html = '<div class="item">\
+			<div class="remove">X</div>' + value + '</div>';
+
+	
+	});
+
+
+
+});
