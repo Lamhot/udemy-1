897108303dc80d1db32d2d4e4d5c52897cfcd5d5
Lamhotjm <lamhotjms@gmail.com>
2020-01-07 15:32:23 -0600

    finished chapter 21

--- src/ajax/chapter21/js/script.js
+++ src/ajax/chapter21/js/script.js
@@ -5,8 +5,17 @@ $(document).ready(function() {
 		var html = '<div class="item">\
 			<div class="remove">X</div>' + value + '</div>';
 
-	
 	});
+	$('#places-container .remove').on('click', function () {
+		$(this).parent().remove();
+
+	})
+
+	// or we have another way to represent
+	$('#places-container').on('click', '.remove', function () {
+		$(this).parent().remove();
+
+	})
 
 
 
