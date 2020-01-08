825dd116cefc2f97b10812d295fc93f2d9d7455f
Lamhotjm <lamhotjms@gmail.com>
2020-01-07 23:10:06 -0600

    cover chap 24 -25

--- src/ajax/chapter23/js/script.js
+++ src/ajax/chapter23/js/script.js
@@ -2,9 +2,16 @@ $(document).ready(function() {
 	$('.item-box').on('click','a.info-link', function (event) {
 		event.preventDefault();
 
-		$(this).closest('.item-box').find('.more-info').toggle('slow',function () {
+		//chapter 23
+		/*$(this).closest('.item-box').find('.more-info').toggle('slow',function () {
         alert("hi");
-		})
+		})*/
+		//chapter 24
+		//$(this).closest('.item-box').find('.more-info').fadeToggle('slow');
+
+		//chapter 25 use slide
+		$(this).closest('.item-box').find('.more-info').slideToggle('fast');
+
 
 	})
 });
