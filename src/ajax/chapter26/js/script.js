054eaee80046d6282c0a35319fe466c7eac93b90
Lamhotjm <lamhotjms@gmail.com>
2020-01-07 23:19:11 -0600

    complete chap 26

---
+++ src/ajax/chapter26/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	$('#example1').on('click', function () {
+		$(this).animate({
+			opacity:0.3
+		}, 500,function () {
+			alert('complete!');
+
+		})
+
+	});
+
+	$('#example2').on('click', function () {
+		$(this).animate({
+			left:200,
+			width:40
+		}, 500,function () {
+			alert('complete!');
+
+		})
+
+	});
+
+
+	$('#example3').on('click', function () {
+		$(this).animate({
+			left: '+=20',
+			width:'+=50'
+		}, 500,function () {
+			alert('complete!');
+
+		})
+
+	})
+});
