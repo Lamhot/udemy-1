15f37fb48bbd78837d20c5ee0eaaf66d1699b6f0
Lamhotjm <lamhotjms@gmail.com>
2020-01-08 15:27:14 -0600

    add post

--- src/nodejs/sample/routes/index.js
+++ src/nodejs/sample/routes/index.js
@@ -29,5 +29,98 @@ router.get('/redirect', (req, res) => {
 	res.redirect('https://www.turbo360.co/landing')
 })
 
+router.get('/test', (req, res)=>{
+	res.json({
+		data:'this is a response!'
+	})
+})
+
+router.get('/query',(req, res) =>
+{
+	const name = req.query.name
+	const occupation = req.query.occupation
+	const data ={
+		name:name,
+		occupation:occupation
+	}
+
+	res.render('profile', data)
+})
+
+router.get('/:path',(req, res) =>
+{
+	const path = req.params.path
+	res.json({
+		data: path
+	})
+})
+
+const profiles =
+	{
+		dkwon:{
+			name: 'dan kwon',
+			company: 'self',
+			languages: ['javascript', 'swift','python']
+		}
+		,
+		dkwon2:{
+			name: 'dan kwon',
+			company: 'self',
+			languages: ['javascript', 'swift','python']
+		},
+		dkwon3:{
+			name: 'dan kwon',
+			company: 'self',
+			languages: ['javascript', 'swift','python']
+		}
+
+	}
+
+	router.get('/:profile/:username', (req,res) =>
+	{
+		const profile = req.params.profile
+		const username =req.params.username
+
+		const cprofile = profiles[username]
+
+		if(cprofile== null)
+		{
+			res.json({
+				confirmation:'fail',
+				message: 'Profile ' + username + ' Not found'
+			})
+			return
+		}
+
+		// res.json(
+		// 	{
+		// 		confirmation:'success',
+		// 		profile: cprofile
+		// 	}
+		// )
+		res.render('profile', cprofile)
+	})
+
+
+router.post('/addprofile',(req, res)=>
+{
+	const body= req.body
+	res.json({
+		confirmation: 'success',
+		data: body
+	})
+})
+router.post('/post', (req, res)=>{
+	const body = req.body
+	body['languages'] = req.body.languages.split(',')
+	res.json({
+		confirmation:'success',
+		data: body
+	})
+})
+
+
+
+
 
 module.exports = router
---
+++ src/nodejs/sample/routes/register.js
@@ -0,0 +1,15 @@
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+router.post('/user', (req, res)=>
+{
+    const body = req.body
+    res.json({
+        confirmation:'success',
+        data: body
+    })
+
+})
+
+module.exports= router
